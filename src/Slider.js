341a7444471d06a3e97b195c2262998307895fa0
normuradoff <normuradoff1@gmail.com>
2021-08-16 13:16:17 +0500

    install typescript

--- src/Slider.js
+++ src/Slider.js
@@ -1,64 +1,79 @@
-class Slide {
-  constructor(text, color, isActive) {
-    this.text = text;
-    this.color = color;
-    this.isActive = isActive;
-  }
-  render() {
-    const slide = document.createElement("div");
-    slide.innerHTML = `<span>${this.text}</span>`;
-    slide.className = "slide";
-    if (this.isActive) {
-      slide.classList.add("active");
+"use strict";
+exports.__esModule = true;
+exports.Slider = void 0;
+var Slide = /** @class */ (function () {
+    function Slide(text, color, isActive) {
+        this.text = text;
+        this.color = color;
+        this.isActive = isActive;
     }
-    slide.style.backgroundColor = this.color;
-
-    return slide;
-  }
-}
-
-export class Slider {
-  constructor({ delay = 2000, root, width = 500, height = 250, slides }) {
-    if (!root) {
-      throw new Error("'root' is a required field");
+    Slide.prototype.render = function () {
+        var slide = document.createElement("div");
+        slide.innerHTML = "<span>" + this.text + "</span>";
+        slide.className = "slide";
+        if (this.isActive) {
+            slide.classList.add("active");
+        }
+        slide.style.backgroundColor = this.color;
+        return slide;
+    };
+    return Slide;
+}());
+var Slider = /** @class */ (function () {
+    function Slider(_a) {
+        var _b = _a.delay, delay = _b === void 0 ? 2000 : _b, root = _a.root, _c = _a.width, width = _c === void 0 ? 500 : _c, _d = _a.height, height = _d === void 0 ? 250 : _d, slides = _a.slides;
+        if (!root) {
+            throw new Error("'root' is a required field");
+        }
+        if (!slides) {
+            throw new Error("'slides' is a required field ");
+        }
+        this.delay = delay;
+        this.root = root;
+        this.width = width;
+        this.height = height;
+        this.slides = slides;
+        //uncomment a line below to render a slider on initialization
+        // this.render();
     }
-    if (!slides) {
-      throw new Error("'slides' is a required field ");
-    }
-
-    this.delay = delay;
-    this.root = root;
-    this.width = width;
-    this.height = height;
-    this.slides = slides;
-
-    //uncomment a line below render a slider on initialization
-    // this.render();
-  }
-
-  changeSlide() {
-    const slideElements = document.querySelectorAll(".slide");
-    slideElements[++this.activeSlideIndex].classList.add("active");
-
-    if (this.activeSlideIndex < slideElements.length - 1) {
-      setTimeout(this.changeSlide.bind(this), this.delay);
-    }
-  }
-
-  render() {
-    const renderHook = document.querySelector(this.root);
-    const slider = document.createElement("div");
-    slider.style.width = `${this.width}px`;
-    slider.style.height = `${this.height}px`;
-    slider.className = "slider";
-
-    this.slides.forEach((slide, index) => {
-      const slideElement = new Slide(slide.text, slide.color, index === 0);
-      slider.append(slideElement.render());
-      this.activeSlideIndex = 0;
-    });
-
-    renderHook.append(slider);
-    setTimeout(this.changeSlide.bind(this), this.delay);
-  }
-}
+    Slider.prototype.changeSlide = function () {
+        var slideElements = document.querySelectorAll(".slide");
+        var previousSlideIndex = this.activeSlideIndex;
+        this.activeSlideIndex = previousSlideIndex + 1;
+        slideElements[this.activeSlideIndex].classList.add("active");
+        window.addEventListener("blur", function () {
+            console.log("blur");
+        });
+        // Remove an active class from previous slide after swipe effect has finished
+        // setTimeout(() => {
+        //   slideElements[previousSlideIndex].classList.remove("active");
+        // }, 400); //400ms here are equal to 400ms in transition-duration property of .slide class in styles file
+        //Change a slide after delay if the active slide is not the last one
+        if (this.activeSlideIndex < slideElements.length - 1) {
+            this.activeTimer = setTimeout(this.changeSlide.bind(this), this.delay);
+        }
+    };
+    Slider.prototype.pauseSlider = function () {
+        clearTimeout(this.activeTimer);
+    };
+    Slider.prototype.resumeSlider = function () {
+        this.activeTimer = setTimeout(this.changeSlide.bind(this), this.delay);
+    };
+    Slider.prototype.render = function () {
+        var _this = this;
+        var renderHook = document.querySelector(this.root);
+        var slider = document.createElement("div");
+        slider.style.width = this.width + "px";
+        slider.style.height = this.height + "px";
+        slider.className = "slider";
+        this.slides.forEach(function (slide, index) {
+            var slideElement = new Slide(slide.text, slide.color, index === 0);
+            slider.append(slideElement.render());
+            _this.activeSlideIndex = 0;
+        });
+        renderHook.append(slider);
+        setTimeout(this.changeSlide.bind(this), this.delay);
+    };
+    return Slider;
+}());
+exports.Slider = Slider;
