22fde059bddb8c5b5aad0c624be761f98f027df1
normuradoff <normuradoff1@gmail.com>
2021-08-16 13:16:05 +0500

    add an eventlistener to pause animation when tab is hidden

--- src/index.js
+++ src/index.js
@@ -49,7 +49,7 @@ const options = {
    * Delay for slide switching
    * @type {number}
    */
-  delay: 2500,
+  delay: 2000,
 
   /**
    * Root element css selector
@@ -144,3 +144,16 @@ const options = {
  */
 const slider = new Slider(options);
 slider.render();
+
+//pause swipe animation when the tab becomes inactive for the cases when delay is not a whole second
+//https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/setTimeout#Timeouts_in_inactive_tabs_clamped_to_%3E1000ms
+const handleVisibility = () => {
+  if (document.visibilityState === "visible") {
+    slider.resumeSlider();
+  } else {
+    slider.pauseSlider();
+  }
+};
+if (options.delay % 1000 !== 0) {
+  document.addEventListener("visibilitychange", handleVisibility);
+}
