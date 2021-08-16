3a3aa57482e50535cc2827447f822fcf51ddeb3f
normuradoff <normuradoff1@gmail.com>
2021-08-16 15:15:18 +0500

    remove helper class from Slider.ts fiel

--- src/Slider.ts
+++ src/Slider.ts
@@ -1,22 +1,3 @@
-class Slide {
-  constructor(
-    private text: string,
-    private color: string,
-    private isActive: boolean
-  ) {}
-
-  render() {
-    const slide = document.createElement("div");
-    slide.innerHTML = `<span>${this.text}</span>`;
-    slide.className = "slide";
-    if (this.isActive) {
-      slide.classList.add("active");
-    }
-    slide.style.backgroundColor = this.color;
-    return slide;
-  }
-}
-
 type SliderOptions = {
   delay: number;
   root: string;
@@ -66,7 +47,7 @@ export class Slider {
     // Remove an active class from previous slide after swipe effect has finished
     setTimeout(() => {
       slideElements[previousSlideIndex].classList.remove("active");
-    }, 400); //400ms here are equal to 400ms in transition-duration property of .slide class in styles file
+    }, 1000);
 
     //Change a slide after delay if the active slide is not the last one
     if (this.activeSlideIndex < slideElements.length - 1) {
@@ -91,8 +72,14 @@ export class Slider {
     slider.className = "slider";
 
     this.slides.forEach((slide, index) => {
-      const slideElement = new Slide(slide.text, slide.color, index === 0);
-      slider.append(slideElement.render());
+      const slideElement = document.createElement("div");
+      slideElement.innerHTML = `<span>${slide.text}</span>`;
+      slideElement.className = "slide";
+      if (index === 0) {
+        slideElement.classList.add("active");
+      }
+      slideElement.style.backgroundColor = slide.color;
+      slider.append(slideElement);
     });
 
     renderHook.append(slider);
